a4075eb81bc02c4ab3ad6b67ac0c2c428de45aac
Juan Mo <jamseams@gmail.com>
2014-09-09 17:43:14 +0200

    Update update_HDD.sh

--- update_HDD.sh
+++ update_HDD.sh
@@ -2,14 +2,15 @@
 
 #This script is to mirror the content of Secondary into the corresponding portable hard disk drive.
 
-CURRENT_DIR=/media/jamseams/Secondary
-UPDATE_DIR1=/media/jamseams/Seagate
-UPDATE_DIR2=/media/jamseams/Passport
+CURRENT_DIR=/media/user/Secondary
+UPDATE_DIR1=/media/user/Seagate
+UPDATE_DIR2=/media/user/Passport
 
 #Check if the Seagate drive is present. In other words, mounted.
 if [ -d "$UPDATE_DIR1" ] && [ -d "$CURRENT_DIR" ]
 then
 	echo "Seagate Backup Drive was found correctly!"
+	#updates the folder with the rsync function while excluding certain folders.
 	sudo rsync -gloptvru --delete --exclude '*.tib' --exclude 'System Volume Information' --exclude '*RECYCLE*' $CURRENT_DIR/* $UPDATE_DIR1
 fi
 
@@ -17,6 +18,7 @@ fi
 if [ -d "$UPDATE_DIR2" ] && [ -d "$CURRENT_DIR" ]
 then
 	echo "Jams Passport was found correctly!"
+	#updates the folder with the rsync function while excluding certain folders.
 	sudo rsync -gloptvru --delete --exclude '*.tib' --exclude 'System Volume Information' --exclude '*RECYCLE*' $CURRENT_DIR/* $UPDATE_DIR2
 fi
 
